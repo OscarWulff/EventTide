ed9d6872ac476981baf731a3cade11bf7415fced
frontendvictor <victoralbert@live.dk>
2024-06-05 16:49:11 +0200

    yupp

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,64 @@ class EventTideApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const LoginPage(),
+        '/': (context) => const MainNavigationWrapper(),
         '/signup': (context) => const SignUpPage(),
         '/swipe': (context) => const SwipePage(),
         '/calendar': (context) => CalendarPage(),
         '/make_event': (context) => const MakeEventPage(),
         '/preview_event': (context) => const PreviewEventPage(),
       },
+    );
+  }
+}
+
+class MainNavigationWrapper extends StatefulWidget {
+  const MainNavigationWrapper({Key? key}) : super(key: key);
+
+  @override
+  _MainNavigationWrapperState createState() => _MainNavigationWrapperState();
+}
+
+class _MainNavigationWrapperState extends State<MainNavigationWrapper> {
+  int _selectedIndex = 1; // Default to 'make' page
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _pages = <Widget>[
+    SwipePage(),
+    MakeEventPage(),
+    CalendarPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromRGBO(222, 121, 46, 1),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.swap_horiz),
+            label: 'Swipe',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Make',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
--- lib/pages/make_event_page.dart
+++ lib/pages/make_event_page.dart
@@ -1,50 +1,112 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'select_weekdays.dart';
 
-class MakeEventPage extends StatelessWidget {
-  const MakeEventPage({super.key});
+class MakeEventPage extends StatefulWidget {
+  const MakeEventPage({Key? key}) : super(key: key);
+
+  @override
+  _MakeEventPageState createState() => _MakeEventPageState();
+}
+
+class _MakeEventPageState extends State<MakeEventPage> {
+  TimeOfDay _selectedTime = TimeOfDay.now();
+  List<DayInWeek> _days = [
+    DayInWeek("Sun", isSelected: false),
+    DayInWeek("Mon", isSelected: false),
+    DayInWeek("Tue", isSelected: false),
+    DayInWeek("Wed", isSelected: false),
+    DayInWeek("Thu", isSelected: false),
+    DayInWeek("Fri", isSelected: false),
+    DayInWeek("Sat", isSelected: false),
+  ];
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child!,
+        );
+      },
+    );
+    if (picked != null && picked != _selectedTime) {
+      setState(() {
+        _selectedTime = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange,
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(222, 121, 46, 1),
         title: const Text('Make Event'),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Container(
-              color: Colors.black,
-              child: const Column(  // Correct syntax: child: Column(...)
-               children: [
-                 Text( //Images
-                    style: TextStyle(color: Colors.white),
-                    'Upload images to describe your event',
-                      ),
-                     
-               TextField( //Title of event
-                    style: TextStyle(color: Colors.white),
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Upload image to describe your event or camp',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Image.asset('assets/Roskilde_logo.png'), // Your image
+                  TextField(
+                    style: TextStyle(color: Colors.black),
                     decoration: InputDecoration(
                       hintText: 'Title of event',
                       hintStyle: TextStyle(color: Colors.grey),
                     ),
                   ),
-                 TextField( //Description of event
+                  ElevatedButton(
+                    onPressed: () => _selectTime(context),
+                    child: Text('Select Time'),
+                  ),
+                  Text(
+                    'Selected Time: ${_selectedTime.format(context)}',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  TextField(
                     style: TextStyle(color: Colors.white),
                     decoration: InputDecoration(
                       hintText: 'Describe your event',
                       hintStyle: TextStyle(color: Colors.grey),
                     ),
                   ),
-                  TextField( //Max number people
+                  TextField(
                     style: TextStyle(color: Colors.white),
                     decoration: InputDecoration(
                       hintText: 'Maximum number of people',
                       hintStyle: TextStyle(color: Colors.grey),
                     ),
-                    keyboardType: TextInputType.number, //Only numbers as input
+                    keyboardType: TextInputType.number, // Only numbers as input
+                  ),
+                  SelectWeekDays(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    days: _days,
+                    border: false,
+                    boxDecoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30.0),
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        colors: [Colors.orange, const Color.fromARGB(255, 204, 127, 12)],
+                        tileMode: TileMode.repeated, // repeats the gradient over the canvas
+                      ),
+                    ),
+                    onSelect: (values) {
+                      print(values);
+                    },
                   ),
                 ],
               ),
@@ -53,7 +115,7 @@ class MakeEventPage extends StatelessWidget {
               onPressed: () {
                 Navigator.pushNamed(context, '/preview_event');
               },
-              child: const Text('Go to Preview Event'),
+              child: const Text('Preview Event'),
             ),
           ],
         ),
