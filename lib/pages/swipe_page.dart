f8458941e5b90b3370927f7ed3174afc280dd261
NielsBown <nielsmollbown@gmail.com>
2024-06-11 14:55:09 +0200

    Update swipe_page.dart

--- lib/pages/swipe_page.dart
+++ lib/pages/swipe_page.dart
@@ -3,8 +3,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:card_swiper/card_swiper.dart';
 
-class SwipePage extends StatelessWidget {
+class SwipePage extends StatefulWidget {
   const SwipePage({Key? key}) : super(key: key);
+
+  @override
+  _SwipePageState createState() => _SwipePageState();
+}
+
+class _SwipePageState extends State<SwipePage> {
+  final SwiperController swiperController = SwiperController();
+  late Future<List<QueryDocumentSnapshot>> _eventsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _eventsFuture = _fetchEvents();
+  }
 
   Future<void> _joinEvent(BuildContext context, String eventId) async {
     final user = FirebaseAuth.instance.currentUser;
@@ -22,6 +36,9 @@ class SwipePage extends StatelessWidget {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Successfully joined the event')),
         );
+        setState(() {
+          _eventsFuture = _fetchEvents();
+        });
       } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Failed to join the event')),
@@ -79,7 +96,7 @@ class SwipePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       body: FutureBuilder<List<QueryDocumentSnapshot>>(
-        future: _fetchEvents(),
+        future: _eventsFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
@@ -95,95 +112,105 @@ class SwipePage extends StatelessWidget {
               final imageUrl = event.containsKey('imageUrl') ? event['imageUrl'] : '';
               final eventId = events[index].id;
 
-              return Card(
-                child: Stack(
-                  children: [
-                    if (imageUrl.isNotEmpty)
+              return GestureDetector(
+                onHorizontalDragEnd: (DragEndDetails details) {
+                  if (details.primaryVelocity != null && details.primaryVelocity! > 0) {
+                    _joinEvent(context, eventId);
+                    swiperController.next();
+                  }
+                },
+                child: Card(
+                  child: Stack(
+                    children: [
+                      if (imageUrl.isNotEmpty)
+                        Positioned.fill(
+                          child: Image.network(
+                            imageUrl,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       Positioned.fill(
-                        child: Image.network(
-                          imageUrl,
-                          fit: BoxFit.cover,
+                        child: Container(
+                          color: Colors.black.withOpacity(0.5), // Optional: Add a dark overlay for better text visibility
                         ),
                       ),
-                    Positioned.fill(
-                      child: Container(
-                        color: Colors.black.withOpacity(0.5), // Optional: Add a dark overlay for better text visibility
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              event['EventTitle'],
+                              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Camp: ${event['CampName']}',
+                              style: TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                            Text(
+                              'Description: ${event['EventDescription']}',
+                              style: TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                            Text(
+                              'Events People Capacity: ${event['MaxPeople']}',
+                              style: TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                            Text(
+                              'Start Time: ${event['StartTime']}',
+                              style: TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                            Text(
+                              'End Time: ${event['EndTime']}',
+                              style: TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            event['EventTitle'],
-                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            'Camp: ${event['CampName']}',
-                            style: TextStyle(fontSize: 18, color: Colors.white),
-                          ),
-                          Text(
-                            'Description: ${event['EventDescription']}',
-                            style: TextStyle(fontSize: 18, color: Colors.white),
-                          ),
-                          Text(
-                            'Events People Capacity: ${event['MaxPeople']}',
-                            style: TextStyle(fontSize: 18, color: Colors.white),
-                          ),
-                          Text(
-                            'Start Time: ${event['StartTime']}',
-                            style: TextStyle(fontSize: 18, color: Colors.white),
-                          ),
-                          Text(
-                            'End Time: ${event['EndTime']}',
-                            style: TextStyle(fontSize: 18, color: Colors.white),
-                          ),
-                        ],
+                      Positioned(
+                        bottom: 16,
+                        left: 16,
+                        right: 16,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.black.withOpacity(0.5),
+                              ),
+                              child: IconButton(
+                                icon: Icon(Icons.close, color: Colors.red, size: 50),
+                                onPressed: () {
+                                  swiperController.previous();
+                                },
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.black.withOpacity(0.5),
+                              ),
+                              child: IconButton(
+                                icon: Icon(Icons.check, color: Colors.green, size: 50),
+                                onPressed: () {
+                                  _joinEvent(context, eventId);
+                                  swiperController.next();
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Positioned(
-                      bottom: 16,
-                      left: 16,
-                      right: 16,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.black.withOpacity(0.5),
-                            ),
-                            child: IconButton(
-                              icon: Icon(Icons.close, color: Colors.red, size: 50),
-                              onPressed: () {
-                                // No action
-                              },
-                            ),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.black.withOpacity(0.5),
-                            ),
-                            child: IconButton(
-                              icon: Icon(Icons.check, color: Colors.green, size: 50),
-                              onPressed: () {
-                                _joinEvent(context, eventId);
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               );
             },
             layout: SwiperLayout.STACK,
             itemHeight: MediaQuery.of(context).size.height,
             itemWidth: MediaQuery.of(context).size.width,
+            controller: swiperController,
           );
         },
       ),
